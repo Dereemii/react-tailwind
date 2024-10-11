ccd9b70906be7412b9518b768c170afc77ab0594
Leslie Herrera - Dereemii <62083149+Dereemii@users.noreply.github.com>
2024-10-11 00:07:46 +0000

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Hola</h1>
+     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</button>
+    </>
+  )
+}
+
+export default App
---
+++ my-project/tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        success: {
+          DEFAULT: '#14a44d',
+          600: '#0d8a3a',
+          700: '#0a682c',
+        },
+        danger: {
+          DEFAULT: '#dc4c64',
+          600: '#c43c52',
+          700: '#a13344',
+        },
+        warning: {
+          DEFAULT: '#e4a11b',
+          600: '#d09419',
+          700: '#b37a14',
+        },
+        info: {
+          DEFAULT: '#54b4d3',
+          600: '#469ab7',
+          700: '#3a8199',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
